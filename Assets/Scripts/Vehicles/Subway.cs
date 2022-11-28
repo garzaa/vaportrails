4edbe622fd23b07be98a41e212362e2501ab0822
Adrian Garza <adrianhgarza@gmail.com>
2022-11-27 23:41:46 -0800

    subway arriving with board

--- Assets/Scripts/Vehicles/Subway.cs
+++ Assets/Scripts/Vehicles/Subway.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Subway : MonoBehaviour {
+
+	public enum SubwayDirection {
+		LEFT = 0,
+		RIGHT = 1,
+	}
+
+	public string stopDestination;
+	public string lineName;
+	public Text infoText;
+
+	public SubwayDirection leaveDirection;
+	public SubwayDirection arriveDirection;
 
 	public GameObject playerDummy;
 	GameObject player;
@@ -10,37 +23,73 @@ public class Subway : MonoBehaviour {
 	SubwayCar[] cars;
 	Animator animator;
 
-	bool arriving = false;
-	bool playerInStation = false;
+	bool holdingPlayer;
+
+	int cycleLength = 10 + 10 + 10 + 10;
 
 	void Start() {
 		cars = GetComponentsInChildren<SubwayCar>();
 		animator = GetComponent<Animator>();
 		player = PlayerInput.GetPlayerOneInput().gameObject;
 		playerDummy.SetActive(false);
+		animator.SetFloat("LeaveDirection", (float) leaveDirection);
+		animator.SetFloat("ArriveDirection", (float) arriveDirection);
+		StartCoroutine(SubwayRoutine());
 	}
 
-	// called as an event from player trigger
-	public void OnPlayerEnterTrigger() {
-		playerInStation = true;
-		ArriveAtStation();
+	IEnumerator SubwayRoutine() {
+		for (;;) {
+			Debug.Log("new subway loop");
+			ArriveAtStation();
+			SetInfoText(NextStopTime(0));
+			// arrive time + wait at station time
+			yield return new WaitForSeconds(10);
+			LeaveStation();
+			SetInfoText(NextStopTime(30));
+			yield return new WaitForSeconds(10);
+			SetInfoText(NextStopTime(20));
+			yield return new WaitForSeconds(10);
+			SetInfoText(NextStopTime(10));
+			yield return new WaitForSeconds(10);
+		}
+	}
+
+	void SetInfoText(string s) {
+		StartCoroutine(UpdateBoard(s.Split('\n')));
+	}
+
+	IEnumerator UpdateBoard(string[] s) {
+		infoText.text = s[0];
+		yield return new WaitForSeconds(0.2f);
+		infoText.text = s[0] + "\n" + s[1];
+		yield return new WaitForSeconds(0.2f);
+		infoText.text = s[0] + "\n" + s[1] + "\n" + s[2];
+	}
+
+	string NextStopTime(int time) {
+		string top = "";
+		if (time == 0) top = RouteInfo() + " now arriving";
+		else top = RouteInfo() + ": " + time+"s";
+		string middle = RouteInfo() + ": " + FormatSeconds(time+cycleLength);
+		string bottom = RouteInfo() + ": " + FormatSeconds(time+cycleLength+cycleLength);
+		return top + "\n" + middle + "\n" + bottom;
+	}
+
+	string RouteInfo() {
+		return $"{lineName} line to {stopDestination}";
+	}
+
+	string FormatSeconds(int seconds) {
+		if (seconds < 60) return seconds+"s";
+		return (seconds / 60)+"m " + (seconds % 60)+"s"; 
 	}
 
 	void ArriveAtStation() {
 		animator.SetTrigger("Arrive");
-		arriving = true;
-	}	
-
-	public void OnPlayerLeaveTrigger() {
-		if (arriving) return;
-		playerInStation = false;
-		LeaveStation();
 	}
 
 	public void LeaveStation() {
-		// called if either player steps out of volume or transitionmanager says to leave
 		animator.SetTrigger("Depart");
-		animator.ResetTrigger("Arrive");
 	}
 
 	public void LoadRidingPlayer(Transition.SubwayTransition subwayTransition) {
@@ -57,24 +106,13 @@ public class Subway : MonoBehaviour {
 		);
 		// show player dummy
 		playerDummy.SetActive(true);
-	}
-
-	public void FinishArriveAnimation() {
-		arriving = false;
-		foreach (SubwayCar car in cars) {
-			car.OpenDoors();
-		}
-		if (!playerInStation) {
-			LeaveStation();
-		}
-		player.SetActive(true);
-		playerDummy.SetActive(false);
+		holdingPlayer = true;
 	}
 
 	public void FinishDepartAnimation() {
-		if (playerInStation) {
-			ArriveAtStation();
-		}
+		if (holdingPlayer) {
+			// set the subway transition and DO IT
+		} 
 	}
 
 	// called from interaction
@@ -91,6 +129,20 @@ public class Subway : MonoBehaviour {
 		);
 		// show player dummy
 		playerDummy.SetActive(true);
+		holdingPlayer = true;
+	}
+
+	void OffboardPlayer() {
+		player.SetActive(true);
+		playerDummy.SetActive(false);
+	}
+
+	// called from arrriving animation
+	// TODO: set this
+	public void OpenAllDoors() {
+		foreach (SubwayCar car in cars) {
+			car.OpenDoors();
+		}
 	}
 
 	// called from departing animation
