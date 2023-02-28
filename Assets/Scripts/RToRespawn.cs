de193c29067498ba91335b82e35b170b3703f934
Adrian Garza <adrianhgarza@gmail.com>
2023-02-27 23:59:56 -0800

    r to restart target run

--- Assets/Scripts/RToRespawn.cs
+++ Assets/Scripts/RToRespawn.cs
@@ -6,6 +6,16 @@ public class RToRespawn : MonoBehaviour {
 	GameObject player;
 
 	void Start() {
-		
+		player = PlayerInput.GetPlayerOneInput().gameObject;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			player.transform.position = transform.position;
+			player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			if (!player.GetComponent<Entity>().facingRight) {
+				player.GetComponent<Entity>().Flip();
+			}
+		}
 	}
 }
