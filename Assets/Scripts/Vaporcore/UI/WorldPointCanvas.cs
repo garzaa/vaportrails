75cfc031c29038b1aed2f789aa50c538042a5361
Adrian Garza <adrianhgarza@gmail.com>
2023-02-27 21:49:44 -0800

    screenspace andromeda warning and better drop

--- Assets/Scripts/Vaporcore/UI/WorldPointCanvas.cs
+++ Assets/Scripts/Vaporcore/UI/WorldPointCanvas.cs
@@ -6,12 +6,17 @@ public class WorldPointCanvas : MonoBehaviour {
 
     new Camera camera;
     public Vector2 position;
+
+    [Tooltip("Use this instead of writing a vector2 position.")]
     public GameObject optionalAnchor;
     RectTransform r;
     Vector3 currentVelocity;
     float maxSpeed = 1000;
     public float smoothTime;
     Rigidbody2D rb2d;
+
+    public bool clampToScreen = false;
+    Vector2 newPos = Vector2.zero;
 
     void Start() {
         camera = Camera.main;
@@ -29,7 +34,17 @@ public class WorldPointCanvas : MonoBehaviour {
                 position = optionalAnchor.transform.position;
             }
         }
-        r.position = Vector3.SmoothDamp(r.position, camera.WorldToScreenPoint(position), ref currentVelocity, smoothTime, maxSpeed);
-        // r.position = camera.WorldToScreenPoint(position);
+
+        if (smoothTime >= 0) {
+            r.position = Vector3.SmoothDamp(r.position, camera.WorldToScreenPoint(position), ref currentVelocity, smoothTime, maxSpeed);
+        } else {
+            r.position = camera.WorldToScreenPoint(position);
+        }
+
+        if (clampToScreen) {
+            newPos.x = Mathf.Clamp(r.position.x, r.sizeDelta.x, Screen.width - r.sizeDelta.x);
+            newPos.y = Mathf.Clamp(r.position.y, r.sizeDelta.y, Screen.height - r.sizeDelta.y);
+            r.position = newPos;
+        }
     }
 }
