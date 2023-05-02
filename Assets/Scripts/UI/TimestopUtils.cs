fff4fd3015a5942f496c8cb67575d21287497489
Adrian Garza <adrianhgarza@gmail.com>
2023-05-01 22:24:43 -0700

    gray fly in cutscene

---
+++ Assets/Scripts/UI/TimestopUtils.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TimestopUtils : MonoBehaviour {
+	public void SetTimeScale(float scale) {
+		Time.timeScale = scale;
+	}
+}
