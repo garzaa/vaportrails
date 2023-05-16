af0610a7769293c5f8afae56fa07e29cd2c7d458
Adrian Garza <adrianhgarza@gmail.com>
2023-05-15 21:53:48 -0700

    more cutscene

--- Assets/Scripts/Vaporcore/AnimationEventInterface.cs
+++ Assets/Scripts/Vaporcore/AnimationEventInterface.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 
 public class AnimationEventInterface : MonoBehaviour {
+	
+	public UnityEvent[] events;
+
 	public void RaiseEvent(GameEvent e) {
 		e.Raise();
+	}
+
+	public void RaiseChildSceneEvent(int index) {
+		events[index].Invoke();
 	}
 }
