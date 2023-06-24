8c1256908c5b460e7fe30816f16efc022e39b2f2
Adrian Garza <adrianhgarza@gmail.com>
2023-06-24 13:35:00 -0700

    more refactors

--- Assets/Scripts/Player/SafeGroundSaver.cs
+++ Assets/Scripts/Player/SafeGroundSaver.cs
@@ -20,28 +20,26 @@ public class SafeGroundSaver {
 
 	public void SaveIfPossible() {
 		// make sure there's a free same-elevation block on either side
-		bool groundCheck = DefaultLinecast(entity.transform.position)
-			&& DefaultLinecast(entity.transform.position + Vector3.left)
-			&& DefaultLinecast(entity.transform.position + Vector3.right);
+		// but not a full 3, just in case they're on a safe two-block platform
+		bool groundCheck = CheckHit(entity.transform.position)
+			&& CheckHit(entity.transform.position + Vector3.left*0.7f)
+			&& CheckHit(entity.transform.position + Vector3.right*0.7f);
 
 		// and then make sure there's no environment damage in a 1-block radius around the character
 		bool envDmgCheck = EnviroDamageClearCheck(entity.transform.position, overlapPoints);
 		
 		if (groundCheck && envDmgCheck) {
-			if (entity.gameObject.name == "Val") {
-				Debug.Log("ground safe check succeeded");
-			}
 			data.lastSafeObject = GetGameObject(entity.transform.position).gameObject;
 			data.lastSafeOffset = entity.transform.position - data.lastSafeObject.transform.position;
 		}
 	}
 	
-	public static bool DefaultLinecast(Vector2 start) {
-		RaycastHit2D hit = Physics2D.Linecast(start, start + Vector2.down * 0.7f, Layers.GroundMask);
+	static bool CheckHit(Vector2 start) {
+		RaycastHit2D hit = DefaultLinecast(start);
 		return (hit && Vector2.Angle(hit.normal, Vector2.up) == 0 && !hit.collider.GetComponent<UnsafeGround>());
 	}
 
-	public static bool EnviroDamageClearCheck(Vector2 start, Collider2D[] results) {
+	static bool EnviroDamageClearCheck(Vector2 start, Collider2D[] results) {
 		int numHits = Physics2D.OverlapCircleNonAlloc(start, 1, results);
 		for (int i=0; i<numHits; i++) {
 			if (results[i].GetComponent<EnvironmentHitbox>() != null) {
@@ -51,7 +49,12 @@ public class SafeGroundSaver {
 		return true;
 	}
 
-	public static GameObject GetGameObject(Vector2 start) {
-		return Physics2D.Linecast(start, start + Vector2.down * 0.7f, Layers.GroundMask).collider.gameObject;
+	static GameObject GetGameObject(Vector2 start) {
+		return DefaultLinecast(start).collider.gameObject;
+	}
+
+	static RaycastHit2D DefaultLinecast(Vector2 start) {
+		// go an entire block down in case the player is in the air
+		return Physics2D.Linecast(start, start + Vector2.down * 1f, Layers.GroundMask);
 	}
 }
