1f7ec003384bc41d7927439e4facdbb8d730cc5c
Adrian Garza <adrianhgarza@gmail.com>
2024-01-29 18:14:26 -0800

    added achievement auto-generation

--- render_achievements.sh
+++ render_achievements.sh
@@ -1,13 +1,28 @@
-# for everything in the achievements/images folder
 for FILE in ./Assets/Achievements/Images/*.png; do
-	# magick convert $FILE 
 	FNAME="$(basename $FILE .png)"
 	echo $FNAME
-	# image magick upscale to 256x256 with nearest neighbor scaling
-	# add a purple background, get the hex code
+
+	# upscale to 256x256 with nearest neighbor scaling
+	# overlay it on top of the steam frame
 	# export as jpg in the steam out/ folder with Got
-	magick convert "$FILE" -filter point -background "#211158" -alpha remove -alpha off -resize 256x256 -quality 100% "./Steam/out/${FNAME}Got.JPG"
-	# then open it, desaturate it, and do it again with Gray
-	magick convert "./Steam/out/${FNAME}Got.JPG" -colorspace Gray "./Steam/out/${FNAME}Gray.JPG"
+	magick convert "./Assets/Achievements/steamFrame.png" \
+		"$FILE" \
+		-gravity Center \
+		-composite \
+		-filter point \
+		-resize 256x256 \
+		-quality 100% \
+		"./Steam/out/${FNAME}Got.JPG"
+
+	# do it again with Gray
+	magick convert "./Assets/Achievements/steamFrameGray.png" \
+		"$FILE" \
+		-gravity Center \
+		-composite \
+		-filter point \
+		-colorspace Gray \
+		-resize 256x256 \
+		-quality 100% \
+		"./Steam/out/${FNAME}Gray.JPG"
 done
 
